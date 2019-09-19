57310b0bc94fad93e052bedc3ca454ccff1d02ca
niudunjuhuacha <623050427@qq.com>
2019-09-19 15:35:40 +0800

    9.19

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,14 +1,15 @@
 package com.atguigu.gmall.user.controller;
 
-import com.atguigu.gmall.user.bean.UmsMember;
-import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
-import com.atguigu.gmall.user.service.UserService;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -24,6 +25,8 @@ public class UserController {
     @RequestMapping("getAllUsers")
     @ResponseBody
     public List<UmsMember> getAllUsers(){
+
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         List<UmsMember> list = new ArrayList<UmsMember>();
         list = userService.getAllUsers();
         return list;
